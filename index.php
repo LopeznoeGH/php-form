4751502b202b422fc9bb77fc76054bdff343d912
LopeznoeGH <72465512+LopeznoeGH@users.noreply.github.com>
2020-11-19 11:33:11 -0800

    added more php.php
    added more code

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-echo '<p>this is PHP!</p>';
+//echo '<p>this is PHP!</p>';
+
+if(isset($_GET["FirstName"])){//show feedback
+ echo $_GET["FirstName"] 
+
+}else{//show form
+   echo '
+   <form action="">
+   First Name <input type="text" name="FirstName" />
+   <br />
+   <input type="submit">
+   
+   </form>
+   ';
+} 
+
+
+
+
+
+
+
 ?>
-<p>this is HTML!</p>
+
